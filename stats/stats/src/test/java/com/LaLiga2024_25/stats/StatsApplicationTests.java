6446df4eec712451da183628ea09f1a0091ac38a
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-06-07 20:17:59 -0500

    Step 3: Add embedded H2 test support - Compile: SUCCESS, Tests: SUCCESS

---
+++ stats/stats/src/test/java/com/LaLiga2024_25/stats/StatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LaLiga2024_25.stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
